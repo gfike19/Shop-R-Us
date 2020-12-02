d7a09a87350c419893aef69d9c4154deeb548cea
gfike19 <gfike19@gmail.com>
2020-12-02 16:43:53 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shop_R_Us.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
