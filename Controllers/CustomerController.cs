3c33e24c745006c75f33c2ffe450ba12ad674c33
gfike19 <gfike19@gmail.com>
2020-12-13 02:23:10 -0600

    can sign up and sign in as customer using hashed passwords

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -8,12 +8,15 @@ using Shop_R_Us.Models;
 using Shop_R_Us.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using Shop_R_Us.Data;
+using System.Web;
+using System.Web.Helpers;
 
 namespace Shop_R_Us.Controllers
 {
     public class CustomerController : Controller
     {
         private ShopRusContext context;
+        private List<Customer> customers;
 
         public CustomerController(ShopRusContext shopRusContext)
         {
@@ -31,7 +34,23 @@ namespace Shop_R_Us.Controllers
         {
             if (ModelState.IsValid)
             {
-                return Redirect("/CustomerOrder/OrderHome/");
+                Customer dbCustomer = null;
+                customers = context.Customers.ToList();
+                foreach(Customer customer in customers)
+                {
+                    if(customer.CustomerName.Equals(customerSignIn.CustomerName))
+                    {
+                        dbCustomer = customer;
+                    }
+                }
+
+                bool verifiedPwd = Crypto.VerifyHashedPassword(dbCustomer.Password, customerSignIn.Password);
+
+                if(verifiedPwd)
+                {
+                    return Redirect("/CustomerOrder/OrderHome/");
+                }
+                return View("SignIn", customerSignIn);
             }
 
             return View("SignIn", customerSignIn);
@@ -46,12 +65,26 @@ namespace Shop_R_Us.Controllers
         [HttpPost]
         public IActionResult SignUp(CustomerSignUp customerSignUp)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-
+                customers = context.Customers.ToList();
+                foreach (Customer c in customers)
+                {
+                    if (c.CustomerName.Equals(customerSignUp.Username))
+                    {
+                        return View("SignUp", customerSignUp);
+                    }
+                }
+                string hashPwd = Crypto.HashPassword(customerSignUp.Password);
+                List<CustomerOrder> custOrder = new List<CustomerOrder>();
+                Customer customer = new Customer(customerSignUp.Username, hashPwd, custOrder);
+                context.Customers.Add(customer);
+                context.SaveChanges();
                 return Redirect("/CustomerOrder/OrderHome/");
             }
             return View("SignUp", customerSignUp);
         }
+        
+
     }
 }
--- ViewModels/CustomerSignUp.cs
+++ ViewModels/CustomerSignUp.cs
@@ -11,7 +11,7 @@ namespace Shop_R_Us.ViewModels
         [Required(ErrorMessage ="Username is required")]
         [MinLength (6)]
         [MaxLength (64)]
-        [UsernameValidator]
+        //[UsernameValidator]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
