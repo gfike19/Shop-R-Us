48950e061cee40f1dddb88825bd88c8d13a08bcb
gfike19 <gfike19@gmail.com>
2021-01-14 12:32:54 -0600

    working on adding cart to session

--- Controllers/CustomerOrderController.cs
+++ Controllers/CustomerOrderController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Shop_R_Us.Models;
 using Shop_R_Us.Data;
+using System.Dynamic;
+
 namespace Shop_R_Us.Controllers
 {
     public class CustomerOrderController : Controller
@@ -18,14 +20,23 @@ namespace Shop_R_Us.Controllers
         [HttpGet]
         public IActionResult OrderHome()
         {
-            if(HttpContext.Request.Cookies["signedIn"] == null)
+            if (HttpContext.Request.Cookies["signedIn"] == null)
             {
                 string msg = "Must be signed in to place an order";
                 ViewBag.msg = msg;
                 return Redirect("/Customer/SignIn");
             }
             List<Product> products = context.Product.ToList();
-            return View(products);
+            dynamic model = new ExpandoObject();
+            model.currentProd = products;
+            CustomerOrder co = new CustomerOrder();
+            Microsoft.AspNetCore.Http.CookieOptions options = new Microsoft.AspNetCore.Http.CookieOptions
+            {
+                Secure = true,
+                SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict
+            };
+            //Response.Cookies.Append("cart", co, options);
+            return View(model);
         }
 
         [HttpPost]
